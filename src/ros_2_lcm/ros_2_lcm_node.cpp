1153b5178e073aefe11d6a48be8e54bc2cb2cccc
jelavice <edo.jelavic@gmail.com>
2017-10-25 19:50:58 +0200

    Fixed compile errors.

--- src/ros_2_lcm/ros_2_lcm_node.cpp
+++ src/ros_2_lcm/ros_2_lcm_node.cpp
@@ -9,6 +9,7 @@
 
 
 #include <iostream>
+#include <memory>
 #include <lcm/lcm-cpp.hpp>
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
@@ -20,9 +21,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-//TODO add new lcm type
-//#include "exlcm/example_t.hpp"
-
+//TODO add types and run some inf of message generation
+#include "BinaryBlob.hpp"
 
 void callback(const sensor_msgs::Image::ConstPtr& msg)
 {
@@ -39,15 +39,21 @@ static const std::string OPENCV_WINDOW = "Image window";
    image_transport::ImageTransport it_;
    image_transport::Subscriber image_sub_;
    image_transport::Publisher image_pub_;
+   std::unique_ptr<lcm::LCM> m_lcmPtr;
 
  public:
    ImageConverter()
-     : it_(nh_)
+     : it_(nh_), m_lcmPtr(new lcm::LCM())
    {
      // Subscrive to input video feed and publish output video feed
      image_sub_ = it_.subscribe("/color_camera_node/image_raw", 1,
        &ImageConverter::imageCb, this);
      image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+
+         if(m_lcmPtr->good() == false)
+             throw std::runtime_error("runtime_error");
+
 
      cv::namedWindow(OPENCV_WINDOW);
    }
@@ -59,12 +65,12 @@ static const std::string OPENCV_WINDOW = "Image window";
 
    void imageCb(const sensor_msgs::ImageConstPtr& msg)
    {
-       std::cout << "shit received." << std::endl;
-     cv_bridge::CvImagePtr cv_ptr;
+
+       cv_bridge::CvImagePtr cv_ptr;
      try
      {
        //cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-       cv_ptr = cv_bridge::toCvCopy(msg, "");
+       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
 
      }
      catch (cv_bridge::Exception& e)
@@ -73,16 +79,28 @@ static const std::string OPENCV_WINDOW = "Image window";
        return;
      }
 
-     // Draw an example circle on the video stream
-//     if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-//       cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+
+
+
+
+     //convert image and send it
+     int8_t ranges[15];
+
+     idsc::BinaryBlob image;
+
+
+
+
+
+    m_lcmPtr->publish("pylon_camera_lcm", &image);
+
 
      // Update GUI Window
      cv::imshow(OPENCV_WINDOW, cv_ptr->image);
      cv::waitKey(3);
 
      // Output modified video stream
-     image_pub_.publish(cv_ptr->toImageMsg());
+     //image_pub_.publish(cv_ptr->toImageMsg());
    }
  };
 
