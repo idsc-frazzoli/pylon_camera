9942e60eeb3569a4c21c9c1c85bbcf2fd3a6e923
jelavice <edo.jelavic@gmail.com>
2017-10-26 17:54:19 +0200

    Fixed color issue when displaying

--- src/ros_2_lcm/ros_2_lcm_node.cpp
+++ src/ros_2_lcm/ros_2_lcm_node.cpp
@@ -19,6 +19,12 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+
+#include <image_view/ImageViewConfig.h>
+
+#include <dynamic_reconfigure/server.h>
+
+
 //TODO add types and run some inf of message generation
 #include "BinaryBlob.hpp"
 
@@ -28,23 +34,22 @@ void callback(const sensor_msgs::Image::ConstPtr& msg) {
 
 static const std::string OPENCV_WINDOW = "Image window";
 
-
-void int2charArray(int num, char* array){
+void int2charArray(int num, char* array) {
 
     array[0] = num & 0xff;
-    array[1] = (num>>8)  & 0xff;
-    array[2] = (num>>16) & 0xff;
-    array[3] = (num>>24) & 0xff;
+    array[1] = (num >> 8) & 0xff;
+    array[2] = (num >> 16) & 0xff;
+    array[3] = (num >> 24) & 0xff;
 
 //    std::cout << "int value in bits: " << std::bitset<32>(num) <<"   vs: chars stacked up: " << std::bitset<8>(array[3])
 //            << std::bitset<8>(array[2]) << std::bitset<8>(array[1]) << std::bitset<8>(array[0]) << std::endl;
 }
 
-void addIntToBlob(int num, idsc::BinaryBlob& blob){
+void addIntToBlob(int num, idsc::BinaryBlob& blob) {
 
     char a[4];
     int2charArray(num, a);
-    for (int i=0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
         blob.data.push_back(a[i]);
 
 }
@@ -63,26 +68,29 @@ public:
         image_sub_ = it_.subscribe("/color_camera_node/image_raw", 1, &ImageConverter::imageCb, this);
         image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
-        //cv::namedWindow(OPENCV_WINDOW);
+        cv::namedWindow(OPENCV_WINDOW);
         std::cout << "Initialized." << std::endl;
     }
 
     ~ImageConverter() {
-        //cv::destroyWindow(OPENCV_WINDOW);
+        cv::destroyWindow(OPENCV_WINDOW);
     }
 
     void imageCb(const sensor_msgs::ImageConstPtr& msg) {
 
-        cv_bridge::CvImagePtr cv_ptr;
+        //cv_bridge::CvImagePtr cv_ptr;
+
+        cv_bridge::CvImageConstPtr cv_ptr;
         try {
             //cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8); //photos are still blueish
+            //cv_ptr = cv_bridge::toCvCopy(msg,""); //photos are still blueish
+
+            cv_ptr = cv_bridge::cvtColorForDisplay(cv_bridge::toCvShare(msg), "");
 
         } catch (cv_bridge::Exception& e) {
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
-
 
         //TODO distinguidh different cases depending on the image format
 //        std::cout << "received image, size is: " << cv_ptr->image.rows << " x " << cv_ptr->image.cols << std::endl;
@@ -96,15 +104,11 @@ public:
         if (m_lcmPtr->good() == false)
             throw std::runtime_error("runtime_error");
 
-        //convert image and send it
-
         int sizeInBytes;
         if (cv_ptr->image.isContinuous())
             sizeInBytes = cv_ptr->image.total() * cv_ptr->image.elemSize();
         else
             sizeInBytes = cv_ptr->image.step[0] * cv_ptr->image.rows;
-
-        //std::cout << "Size in bytes: " << sizeInBytes << std::endl;
 
         idsc::BinaryBlob image;
 
@@ -121,9 +125,7 @@ public:
         addIntToBlob(channels, image);
         addIntToBlob(type, image);
 
-
-
-        cv::MatIterator_<cv::Vec3b> it, end;
+        cv::MatConstIterator_<cv::Vec3b> it, end;
         for (it = cv_ptr->image.begin<cv::Vec3b>(), end = cv_ptr->image.end<cv::Vec3b>(); it != end; ++it) {
             unsigned char g = (*it)[0];
             unsigned char r = (*it)[1];
@@ -132,16 +134,14 @@ public:
             image.data.push_back(r);
             image.data.push_back(b);
         }
-        //std::cout << "Vector size: " << image.data.size() << std::endl;
+
         m_lcmPtr->publish("pylon_camera_lcm", &image);
 
         // Update GUI Window
-//        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-//        cv::waitKey(3);
+        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        cv::waitKey(3);
 
-        // Output modified video stream
-        //image_pub_.publish(cv_ptr->toImageMsg());
-        std::cout << "Published" <<std::endl;
+        //std::cout << "Published" <<std::endl;
     }
 };
 
