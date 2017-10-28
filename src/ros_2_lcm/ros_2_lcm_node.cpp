b8448c5cf8a9fdb3ddec714433d9efa8870e38b4
jelavice <edo.jelavic@gmail.com>
2017-10-28 19:43:44 +0200

    Bug fix

--- src/ros_2_lcm/ros_2_lcm_node.cpp
+++ src/ros_2_lcm/ros_2_lcm_node.cpp
@@ -9,17 +9,17 @@
 #include <iostream>
 #include <ros/ros.h>
 
-
 #include "ros_2_lcm/ros_2_lcm.h"
 
-
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "ros_2_lcm");
-    std::string rosTopicName = std::string(argv[1]);
-    std::string lcmTopicName = std::string(argv[2]);
+
+    std::string rosNodeName = std::string(argv[1]);
+    std::string rosTopicName = std::string(argv[2]);
+    std::string lcmTopicName = std::string(argv[3]);
+
+    ros::init(argc, argv, rosNodeName);
     ros2lcm::RosToLcm rosToLcm(rosTopicName, lcmTopicName);
     ros::spin();
     return 0;
 }
-
 
